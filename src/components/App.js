ae4ce2050798c51ab33b7eab8f8e5bc5fc1f3984
goranc <cabarkapa.goran@gmail.com>
2021-04-07 11:30:16 +0200

    Added App's data as a state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-//Import styles
+// Import styles
 import 'styles/app.scss';
-//Import components
+// Import components
 import Player from 'components/Player';
 import Song from 'components/Song';
+// Import Util
+import data from '../data';
 
 const App = () => {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
