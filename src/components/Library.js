778e62bbe8c155f15532316b43648a70540cdc6e
goranc <cabarkapa.goran@gmail.com>
2021-04-10 14:27:36 +0200

    Selecting songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import LibrarySong from 'components/LibrarySong';
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song, index) => (
-          <LibrarySong key={index} song={song} />
+          <LibrarySong
+            key={index}
+            setCurrentSong={setCurrentSong}
+            song={song}
+          />
         ))}
       </div>
     </div>
